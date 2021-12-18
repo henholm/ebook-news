299ad206f5f833f6a34d24e2802ee8f79337a03f
Henrik Holm <henholm@kth.se>
2021-12-18 19:54:04 +0100

    Add if statement

--- update-news.sh
+++ update-news.sh
@@ -4,29 +4,26 @@
 notifier=/usr/local/bin/terminal-notifier
 calibre_folder="/Users/$USER/Calibre Library/calibre/"
 dropbox_folder="/Users/$USER/Dropbox/dropbox_books"
-echo "Searching for news epubs in $calibre_folder. Copying to $dropbox_folder if any are found."
+echo "Searching for epubs in $calibre_folder."
 
-# Delete from Calibre if older than 5 days.
-# find $calibre_folder -mtime +5 -type d -delete
-find "$calibre_folder" -mtime +5 -type d -print
+# Delete from Calibre if older than 5 days. -mindepth ensures we don't delete the $calibre_folder directory.
+find "$calibre_folder" -mindepth 1 -type d -mtime +5 -print -exec rm -rv "{}" \;
 
 # Delete The Economist, FAZ, Le Monde, and 人民日报 from Dropbox if older than 5 days.
-# find $dropbox_folder \( -name "*The Economist*" -o -name "*Le Monde*" -o -name "*FAZ*" -o -name "*人民日报*" \) -mtime -5 -type f -delete
-find "$dropbox_folder" \( -name "*The Economist*" -o -name "*Le Monde*" -o -name "*FAZ*" -o -name "*Ren Min Ri Bao*" \) -mtime -5 -type f -print
+find "$dropbox_folder" \( -name "*The Economist*" -o -name "*Le Monde*" -o -name "*FAZ*" -o -name "*Ren Min Ri Bao*" \) -mtime +5 -type f -delete
 
-# If new news exist, add them to Dropbox. Use -print0 and -0 to handle paths containing spaces.
-# find "$calibre_folder" \( -name "*The Economist*.epub" -o -name "*Le Monde*.epub" -o -name "*FAZ*.epub" -o -name "*Ren Min Ri Bao*.epub" \) -type f -print0 | xargs -0 cp "$dropbox_folder"
-# to_copy_array=$(find "$calibre_folder" \( -name "*The Economist*.epub" -o -name "*Le Monde*.epub" -o -name "*FAZ*.epub" -o -name "*Ren Min Ri Bao*.epub" \) -type f)
+# If new epubs exist, add them to Dropbox. Use -print0 to handle paths containing spaces.
 to_copy_array=()
 while IFS=  read -r -d $'\0'; do
     to_copy_array+=("$REPLY")
 done < <(find "$calibre_folder" \( -name "*The Economist*.epub" -o -name "*Le Monde*.epub" -o -name "*FAZ*.epub" -o -name "*Ren Min Ri Bao*.epub" \) -type f -print0)
 
-# Use the -u / --update flag to only copy if source file does not exist in destination folder, or if source file is newer than existing file in destination folder.
-cp -u "${to_copy_array[@]}" "$dropbox_folder"
-echo "Copied ${#to_copy_array} news epubs to Dropbox."
+# Use the -n flag to not overwrite existing.
+if (( ${#to_copy_array[@]} != 0 )); then
+    cp -n -v "${to_copy_array[@]}" "$dropbox_folder"
+fi
+echo "Copied ${#to_copy_array[@]} epubs to $dropbox_folder."
 
-$notifier -title "Update News" -subtitle "Removing old Calibre News / Adding new news to Dropbox" -message "Completed"
-
-now=$(date)
-echo "Cron job to update news completed at $now"
+# Display a MacOS notification.
+$notifier -title "Update News" -subtitle "Update Calibre / Dropbox news" -message "Completed"
+echo "Cron job to update news completed at $(date)."
